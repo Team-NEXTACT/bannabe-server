746c64052a54de5cefef3072d5bb5e936442e792
BlanC <74227460+kihong1602@users.noreply.github.com>
2025-02-28 00:53:30 +0900

    Feature: 이메일 발송 비동기 처리
    - 인증코드 발송 로직에 @Async를 적용해 응답속도 개선
    - ThreadPoolTaskExecutor 추가 및 설정
    - 동기방식 대비 응답시간 4.5s -> 498ms로 단축

--- server/src/main/java/site/bannabe/server/global/mail/MailService.java
+++ server/src/main/java/site/bannabe/server/global/mail/MailService.java
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -24,6 +25,7 @@ public class MailService {
 
   private final TemplateEngine templateEngine;
 
+  @Async("asyncMailExecutor")
   public void sendAuthCodeMail(String email, String authCode) {
     MimeMessage mimeMessage = mailSender.createMimeMessage();
 

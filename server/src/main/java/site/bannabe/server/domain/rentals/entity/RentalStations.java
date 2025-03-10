b3f6f08b25918a9dfab736ea1e454a5a926cf297
BlanC <74227460+kihong1602@users.noreply.github.com>
2025-03-10 19:26:21 +0900

    Feature: Entity Builder 패턴 추가
    - RandomCodeGenerator에 Token 생성 메서드 추가
    - Repository 테스트를 위해 Build 어노테이션 추가

--- server/src/main/java/site/bannabe/server/domain/rentals/entity/RentalStations.java
+++ server/src/main/java/site/bannabe/server/domain/rentals/entity/RentalStations.java
@@ -3,6 +3,7 @@ package site.bannabe.server.domain.rentals.entity;
 import jakarta.persistence.Entity;
 import java.math.BigDecimal;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import site.bannabe.server.global.type.BaseEntity;
@@ -29,5 +30,20 @@ public class RentalStations extends BaseEntity {
   private StationStatus status;
 
   private StationGrade grade;
+
+  @Builder
+  private RentalStations(String name, String address, BigDecimal latitude, BigDecimal longitude, String openTime,
+      String closeTime,
+      String closeDay, StationStatus status, StationGrade grade) {
+    this.name = name;
+    this.address = address;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.openTime = openTime;
+    this.closeTime = closeTime;
+    this.closeDay = closeDay;
+    this.status = status;
+    this.grade = grade;
+  }
 
 }
